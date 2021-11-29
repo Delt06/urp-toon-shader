a153224f470f8c0891d51fe72efee288dcc000d3
Vladislav Kantaev <vvkantaev@yandex.ru>
2021-11-29 22:46:29 +0300

    Fix implicit truncation warning

--- Packages/com.deltation.toon-shader/Assets/DELTation/ToonShader/Shaders/Fur/ToonShaderFurUtils.hlsl
+++ Packages/com.deltation.toon-shader/Assets/DELTation/ToonShader/Shaders/Fur/ToonShaderFurUtils.hlsl
@@ -13,7 +13,7 @@ void fur_hook_vertex_input(inout appdata input)
 
 void fur_hook_fragment_albedo(const half2 uv, inout half4 albedo)
 {
-    const half mask = SAMPLE_TEXTURE2D(_FurMask, sampler_FurMask, uv);
+    const half mask = SAMPLE_TEXTURE2D(_FurMask, sampler_FurMask, uv).r;
     clip(mask - 1);
     const float4 fur_noise_st = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _FurNoise_ST);
     const half2 noise_uv = apply_tiling_offset(uv, fur_noise_st);
