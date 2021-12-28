c35a4e91ff0b597e2257d1eaef648f35d6fbaedd
Vladislav Kantaev <vvkantaev@yandex.ru>
2021-12-28 19:25:49 +0300

    Change TOON_SHADER_LITE_HOOK_MAIN_LIGHT to expression

--- Packages/com.deltation.toon-shader/Assets/DELTation/ToonShader/Shaders/ToonShaderLiteForwardPass.hlsl
+++ Packages/com.deltation.toon-shader/Assets/DELTation/ToonShader/Shaders/ToonShaderLiteForwardPass.hlsl
@@ -48,10 +48,12 @@ struct v2f
 inline half4 get_main_light_color_and_brightness(in const float4 position_cs, in const half3 normal_ws,
                                                  const float4 shadow_coords = 0)
 {
-    Light main_light = GetMainLight(shadow_coords);
     #ifdef TOON_SHADER_LITE_HOOK_MAIN_LIGHT
-    TOON_SHADER_LITE_HOOK_MAIN_LIGHT(main_light)
+    const Light main_light = TOON_SHADER_LITE_HOOK_MAIN_LIGHT(shadow_coords);
+    #else
+    const Light main_light = GetMainLight(shadow_coords);
     #endif
+    
     const half3 light_direction_ws = normalize(main_light.direction);
     const half main_light_attenuation = main_light.shadowAttenuation * main_light.distanceAttenuation;
     const half brightness = get_brightness(position_cs, normal_ws, light_direction_ws,
