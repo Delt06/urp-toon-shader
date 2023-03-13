9382d9261621ab148f2b67fa0810d94451018667
Vladislav Kantaev <vvkantaev@yandex.ru>
2023-03-13 08:42:20 +0300

    Fix NormalsSmoothingUtility.cs using assertions from NUint instead of from Unity

--- Packages/com.deltation.toon-shader/Assets/DELTation/ToonShader/Editor/NormalsSmoothing/NormalsSmoothingUtility.cs
+++ Packages/com.deltation.toon-shader/Assets/DELTation/ToonShader/Editor/NormalsSmoothing/NormalsSmoothingUtility.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
-using NUnit.Framework;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace DELTation.ToonShader.Editor.NormalsSmoothing
 {
